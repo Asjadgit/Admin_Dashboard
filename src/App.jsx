a4a88619c0814d3972f52e0d18cffc38e0978df8
Asjadgit <naveedasjad840@gmail.com>
2024-08-21 12:20:37 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react"
+import Test from "./components/testing"
+
+export const Welcome = () => {
+  return (
+    <Fragment>
+      <Test />
+    </Fragment>
+  )
+}
---
+++ src/components/testing.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+        </div>
+    )
+}
+
+export default Test
